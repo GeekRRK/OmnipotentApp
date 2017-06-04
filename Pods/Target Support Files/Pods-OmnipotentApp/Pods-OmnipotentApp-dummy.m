c78a4939e96e041e72be3626ee0f91b1884b6f6f
GeekRRK <669672615@qq.com>
2017-06-04 16:03:52 +0800

    Rename project to SwiftLearning

--- Pods/Target Support Files/Pods-OmnipotentApp/Pods-OmnipotentApp-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_OmnipotentApp : NSObject
-@end
-@implementation PodsDummy_Pods_OmnipotentApp
-@end
---
+++ SwiftLearning/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  OmnipotentApp
+//
+//  Created by GeekRRK on 15/11/30.
+//  Copyright © 2015年 GeekRRK. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate, UISplitViewControllerDelegate {
+
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey : Any]? = nil) -> Bool {
+        
+        return false
+    }
+}
+
