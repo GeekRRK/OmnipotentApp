9a29d10cac73400dc66b4d5dfcbcf2720f722c68
GeekRRK <669672615@qq.com>
2017-06-04 10:17:36 +0800

    Finish Artist

---
+++ OmnipotentApp/Learning/Views/ArtistTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  ArtistTableViewCell.swift
+//  OmnipotentApp
+//
+//  Created by Al on 6/4/17.
+//  Copyright © 2017 suorui. All rights reserved.
+//
+
+import UIKit
+
+class ArtistTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var bioLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var artistImageView: UIImageView!
+    
+}
---
+++ OmnipotentApp/Learning/Views/WorkTableViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  WorkTableViewCell.swift
+//  OmnipotentApp
+//
+//  Created by Al on 6/4/17.
+//  Copyright © 2017 suorui. All rights reserved.
+//
+
+import UIKit
+
+class WorkTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var workImageView: UIImageView!
+    @IBOutlet weak var workTitleLabel: UILabel!
+    @IBOutlet weak var moreInfoTextView: UITextView!
+}
