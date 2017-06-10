1a28ab77ccb85c81d56dd0ffa566e03ea5dad581
GeekRRK <669672615@qq.com>
2017-06-10 15:33:38 +0800

    Finish the SnapchatMenu

--- SwiftLearning/Controller/ViewController.swift
+++ SwiftLearning/Controller/ViewController.swift
@@ -9,32 +9,53 @@
 import UIKit
 
 class ViewController: UIViewController {
-    @IBOutlet weak var dotOne: UIImageView!
-    @IBOutlet weak var dotTwo: UIImageView!
-    @IBOutlet weak var dotThree: UIImageView!
+
+    @IBOutlet weak var scrollView: UIScrollView!
+    
+    enum vcName: String {
+        case chat = "ChatViewController"
+        case stories = "StoriesViewController"
+        case discover = "DiscoverViewController"
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        startAnimation()
+        let chatVC = UIViewController(nibName: vcName.chat.rawValue, bundle: nil)
+        let storiesVC = UIViewController(nibName: vcName.stories.rawValue, bundle: nil)
+        let discoverVC = UIViewController(nibName: vcName.discover.rawValue, bundle: nil)
+        add(childViewController: chatVC, toParentViewController: self)
+        add(childViewController: storiesVC, toParentViewController: self)
+        add(childViewController: discoverVC, toParentViewController: self)
+        
+        /// Set up current snap view.
+        let snapView = UIImageView(image: UIImage(named: "Snap"))
+        changeX(ofView: snapView, xPosition: view.frame.width)
+        scrollView.addSubview(snapView)
+        
+        /// Move stories and discover view to the right screen.
+        changeX(ofView: storiesVC.view, xPosition: view.frame.width * 2)
+        changeX(ofView: discoverVC.view, xPosition: view.frame.width * 3)
+        
+        /// Set up contentSize and contentOffset.
+        scrollView.contentSize = CGSize(width: view.frame.width * 4, height: view.frame.height)
+        scrollView.contentOffset.x = view.frame.width
     }
     
-    func startAnimation() {
-        dotOne.transform = CGAffineTransform(scaleX: 0.01, y: 0.01)
-        dotTwo.transform = CGAffineTransform(scaleX: 0.01, y: 0.01)
-        dotThree.transform = CGAffineTransform(scaleX: 0.01, y: 0.01)
-        
-        UIView.animate(withDuration: 0.6, delay: 0.0, options: [.repeat, .autoreverse], animations: { 
-            self.dotOne.transform = CGAffineTransform.identity
-        }, completion: nil)
-        
-        UIView.animate(withDuration: 0.6, delay: 0.2, options: [.repeat, .autoreverse], animations: {
-            self.dotTwo.transform = CGAffineTransform.identity
-        }, completion: nil)
-        
-        UIView.animate(withDuration: 0.6, delay: 0.4, options: [.repeat, .autoreverse], animations: {
-            self.dotThree.transform = CGAffineTransform.identity
-        }, completion: nil)
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+    
+    fileprivate func changeX(ofView view: UIView, xPosition: CGFloat) {
+        var frame = view.frame
+        frame.origin.x = xPosition
+        view.frame = frame
+    }
+    
+    fileprivate func add(childViewController: UIViewController, toParentViewController parentViewController: UIViewController) {
+        addChildViewController(childViewController)
+        scrollView.addSubview(childViewController.view)
+        childViewController.didMove(toParentViewController: parentViewController)
     }
 
 }
