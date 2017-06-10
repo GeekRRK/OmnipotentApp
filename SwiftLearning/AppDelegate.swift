8cea14e7d300764bff929dc9dbb5308998b4ae44
GeekRRK <669672615@qq.com>
2017-06-10 09:29:34 +0800

    Finish the TwitterBird

--- SwiftLearning/AppDelegate.swift
+++ SwiftLearning/AppDelegate.swift
@@ -9,14 +9,55 @@
 import UIKit
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate, CAAnimationDelegate {
 
     var window: UIWindow?
+    var mask: CALayer?
+    var imageView: UIImageView?
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey : Any]? = nil) -> Bool {
+        window = UIWindow(frame: UIScreen.main.bounds)
         
+        if let window = window {
+            imageView = UIImageView(frame: window.frame)
+            imageView!.image = UIImage(named: "twitterScreen")
+            window.addSubview(imageView!)
+            
+            mask = CALayer()
+            mask?.contents = UIImage(named: "twitterBird")?.cgImage
+            mask?.position = window.center
+            mask?.bounds = CGRect(x: 0, y: 0, width: 100, height: 80)
+            imageView!.layer.mask = mask
+            
+            animateMask()
+            
+            window.rootViewController = UIViewController()
+            window.backgroundColor = UIColor(red: 70/255, green: 154/255, blue: 233/255, alpha: 1)
+            window.makeKeyAndVisible()
+        }
         
+        UIApplication.shared.isStatusBarHidden = true
         return true
     }
     
+    func animateMask() {
+        let keyFrameAnimation = CAKeyframeAnimation(keyPath: "bounds")
+        keyFrameAnimation.delegate = self
+        keyFrameAnimation.duration = 1
+        keyFrameAnimation.beginTime = CACurrentMediaTime() + 1
+        
+        let initialBounds = NSValue(cgRect: mask!.bounds)
+        let secondBounds = NSValue(cgRect: CGRect(x: 0, y: 0, width: 80, height: 64))
+        let finalBounds = NSValue(cgRect: CGRect(x: 0, y: 0, width: 2000, height: 2000))
+        keyFrameAnimation.values = [initialBounds, secondBounds, finalBounds]
+        
+        keyFrameAnimation.keyTimes = [0, 0.3, 1]
+        
+        keyFrameAnimation.timingFunctions = [CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseInEaseOut), CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseOut)]
+        mask!.add(keyFrameAnimation, forKey: "bounds")
+    }
+    
+    func animationDidStop(_ anim: CAAnimation, finished flag: Bool) {
+        imageView?.layer.mask = nil
+    }
 }
