46f3689f67e5472b79dbb796abf3be9f55224ea0
GeekRRK <669672615@qq.com>
2017-06-14 15:50:42 +0800

    Finish the Project 20 - FlickrSearch

---
+++ SwiftLearning/Model/FlickrSearchResults.swift
@@ -0,0 +1,14 @@
+//
+//  FlickrSearchResults.swift
+//  SwiftLearning
+//
+//  Created by Al on 6/14/17.
+//  Copyright © 2017 GeekRRK. All rights reserved.
+//
+
+import Foundation
+
+struct FlickrSearchResults {
+    let searchTerm : String
+    let searchResults : [FlickrPhoto]
+}
