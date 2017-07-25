c1fa07b6baf1c77ab9b7210ff0006cf8d6af9cec
GeekRRK <669672615@qq.com>
2017-07-25 18:28:17 +0800

    Deinitialization finished

---
+++ The Swift Programming Language (Swift 4)/15.Deinitialization.playground/Contents.swift
@@ -0,0 +1,53 @@
+// A deinitializer is called immediately before a class instance is deallocated. You write deinitializers with the deinit keyword, similar to how initializers are written with the init keyword. Deinitializers are only available on class types.
+
+// Swift automatically deallocates your instances when they are no longer needed, to free up resources. Swift handles the memory management of instances through automatic reference counting (ARC). Typically you don’t need to perform manual cleanup when your instances are deallocated. However, when you are working with your own resources, you might need to perform some additional cleanup yourself. For example, if you create a custom class to open a file and write some data to it, you might need to close the file before the class instance is deallocated.
+
+// Class definitions can have at most one deinitializer per class. The deinitializer does not take any parameters and is written without parentheses:
+/*
+deinit {
+    // perform the deinitialization
+}
+ */
+
+// Deinitializers are called automatically, just before instance deallocation takes place. You are not allowed to call a deinitializer yourself. Superclass deinitializers are inherited by their subclasses, and the superclass deinitializer is called automatically at the end of a subclass deinitializer implementation. Superclass deinitializers are always called, even if a subclass does not provide its own deinitializer.
+
+// Because an instance is not deallocated until after its deinitializer is called, a deinitializer can access all properties of the instance it is called on and can modify its behavior based on those properties (such as looking up the name of a file that needs to be closed).
+
+class Bank {
+    static var coinsInBank = 10_000
+    static func distribute(coins numberOfCoinsRequested: Int) -> Int {
+        let numberOfCoinsToVend = min(numberOfCoinsRequested, coinsInBank)
+        coinsInBank -= numberOfCoinsToVend
+        return numberOfCoinsToVend
+    }
+    static func receive(coins: Int) {
+        coinsInBank += coins
+    }
+}
+
+class Player {
+    var coinsInPurse: Int
+    init(coins: Int) {
+        coinsInPurse = Bank.distribute(coins: coins)
+    }
+    func win(coins: Int) {
+        coinsInPurse += Bank.distribute(coins: coins)
+    }
+    deinit {
+        Bank.receive(coins: coinsInPurse)
+    }
+}
+
+var playerOne: Player? = Player(coins: 100)
+print("A new player has joined the game with \(playerOne!.coinsInPurse) coins")
+print("There are now \(Bank.coinsInBank) coins left in the bank")
+//  An optional variable is used here, because players can leave the game at any point. The optional lets you track whether there is currently a player in the game.
+
+playerOne!.win(coins: 2_000)
+print("PlayerOne won 2000 coins & now has \(playerOne!.coinsInPurse) coins")
+print("The bank now only has \(Bank.coinsInBank) coins left")
+
+playerOne = nil
+print("PlayerOne has left the game")
+print("The bank now has \(Bank.coinsInBank) coins")
+
