78761e2e43544b5670fe79e5dbab5aca168e2d80
GeekRRK <669672615@qq.com>
2017-07-25 23:12:11 +0800

    Begin to learn Automatic Reference Counting

---
+++ The Swift Programming Language (Swift 4)/16.Automatic Reference Counting.playground/Contents.swift
@@ -0,0 +1,2 @@
+// ARC automatically frees up the memory used by class instances when those instances are no longer needed.
+// Reference counting only applies to instances of classes. Structures and enumerations are value types, not reference types, and are not stored and passed by reference.
