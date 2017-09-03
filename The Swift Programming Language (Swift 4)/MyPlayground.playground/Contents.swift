a996b88e0a4ab53a32ea6f98aa639e5c8b1165f7
GeekRRK <669672615@qq.com>
2017-09-03 16:35:50 +0800

    Reviewing Nested Types finished

--- The Swift Programming Language (Swift 4)/MyPlayground.playground/Contents.swift
+++ The Swift Programming Language (Swift 4)/MyPlayground.playground/Contents.swift
@@ -1,87 +1,39 @@
-class MediaItem {
-    var name: String
-    init(name: String) {
-        self.name = name
+struct BlackjackCard {
+    enum Suit: Character {
+        case spades = "♠", hearts = "♡", diamonds = "♢", clubs = "♣"
+    }
+    
+    enum Rank: Int {
+        case two = 2, three, four, five, six, seven, eight, nine, ten
+        case jack, queen, king, ace
+        struct Values {
+            let first: Int, second: Int?
+        }
+        var values: Values {
+            switch self {
+            case .ace:
+                return Values(first: 1, second: 11)
+            case .jack, .queen, .king:
+                return Values(first: 10, second: nil)
+            default:
+                return Values(first: self.rawValue, second: nil)
+            }
+        }
+    }
+    
+    let rank: Rank, suit: Suit
+    var description: String {
+        var output = "suit is \(suit.rawValue),"
+        output += " value is \(rank.values.first)"
+        if let second = rank.values.second {
+            output += " or \(second)"
+        }
+        return output
     }
 }
 
-class Movie: MediaItem {
-    var director: String
-    init(name: String, director: String) {
-        self.director = director
-        super.init(name: name)
-    }
-}
+let theAceOfSpades = BlackjackCard(rank: .ace, suit: .spades)
+print("theAceOfSpades: \(theAceOfSpades.description)")
 
-class Song: MediaItem {
-    var artist: String
-    init(name: String, artist: String) {
-        self.artist = artist
-        super.init(name: name)
-    }
-}
+let heartsSymbol = BlackjackCard.Suit.hearts.rawValue
 
-let library = [
-    Movie(name: "Casablanca", director: "Michael Curtiz")
-    Song(name: "Blue Suede Shoes", artist: "Elvis Presley"),
-    Movie(name: "Citizen Kane", director: "Orson Welles"),
-    Song(name: "The One And Only", artist: "Chesney Hawkes"),
-    Song(name: "Never Gonna Give You Up", artist: "Rick Astley")
-]
-
-var movieCount = 0
-var songCount = 0
-
-for item in library {
-    if item is Movie {
-        movieCount += 1
-    } else if item is Song {
-        songCount += 1
-    }
-}
-
-print("Media library contains \(movieCount) movies and \(songCount) songs")
-
-for item in library {
-    if let movie = item as? Movie {
-        print("Movie: \(movie.name), dir. \(movie.director)")
-    } else if let song = item as? Song {
-        print("Song: \(song.name), by \(song.artist)")
-    }
-}
-
-var things = [Any]()
-
-things.append(0)
-things.append(0.0)
-things.append(42)
-things.append(3.14159)
-things.append("hello")
-things.append((3.0, 5.0))
-things.append(Movie(name: "Ghostbusters", director: "Ivan Reitman"))
-things.append({ (name: String) -> String in "Hello", \(name) })
-
-for thing in things {
-    switch thing {
-    case 0 as Int:
-        print("zero as an Int")
-    case 0 as Double:
-        print("zero as a Double")
-    case let someInt as Int:
-        print("an Integer value of \(someInt)")
-    case let someDouble as Double where someDouble > 0:
-        print("a positive double value of \(someDouble)")
-    case is Double:
-        print("some other double value that I don't want to print")
-    case let someString as String:
-        print("a string value of \"\(someString)\"")
-    case let (x, y) as (Double, Double):
-        print("an (x, y) print at \(x), \(y)")
-    case let movie as Movie:
-        print("a movie called \(movie.name), dir. \(movie.director)")
-    case let stringConverter as (String) -> String:
-        print(stringConverter("Michael"))
-    default:
-        print("something else")
-    }
-}
